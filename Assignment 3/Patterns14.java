8a239772f50c71771b69502d4022fd86f9eec1ea
rushikeshdj <100847920+rushikeshdj@users.noreply.github.com>
2022-03-19 14:47:00 +0530

    Add files via upload

---
+++ Assignment 3/Patterns14.java
@@ -0,0 +1,30 @@
+class Patterns14{
+	public static void main(String args[]){
+		for(int i=5;i>=1;i--){
+			for(int j=5;j>=i;j--){
+				System.out.print(" ");
+			}
+			
+			for(int k=i;k>=1;k--){
+				System.out.print("* ");
+				
+			}
+			
+			System.out.println();
+		}
+		for(int i=4;i>=1;i--){
+			for(int j=1;j<=i;j++){
+				System.out.print(" ");
+			}
+			
+			for(int k=i;k<=5;k++){
+				System.out.print("* ");
+				
+			}
+			
+			System.out.println();
+		}
+		
+		
+	}
+}
